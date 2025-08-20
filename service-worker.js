d5eb037744573a6c07eafed50e98a4b7340622ad
albertopc <alberto88pc@gmail.com>
2025-08-20 18:29:32 +0200

    Actualización: guardados cambios en manifest, iconos y estilos

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "rutina-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting(); // fuerza a instalar la nueva versión inmediatamente
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => caches.delete(cache)) // borra cachés antiguas
+      )
+    )
+  );
+  self.clients.claim(); // toma control de todas las pestañas abiertas
+});
